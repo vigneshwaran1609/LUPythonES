ab9083773acf4b0ab8ca9d38b5aa2efaac7bd8c6
vigneshwaran1609 <66218769+vigneshwaran1609@users.noreply.github.com>
2020-07-22 10:52:52 +0530

    Add files via upload

---
+++ Day2_Assignment_PythonEs.py.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# # Basic syntax
+
+# In[5]:
+
+
+print('Hello \n World') #To print new line
+print("hello\tWorld")  #To leave space in the line
+print('I\'m  always a king for me.') #To add single qoute inside your code
+
+
+# # Variables and Assignment Statements
+
+# In[21]:
+
+
+a= 10
+print(type(a))
+print(a)
+b=10.00
+print(type(b))
+print(b)
+c="Python Essentials Day2"
+print(type(c))
+print(c)
+
+
+# # Python Operators
+
+# In[24]:
+
+
+a=int(input())
+b=int(input())
+print("The sum of ",a+b)
+print("The difference of ",a-b)
+print("The muliplication of",a*b)
+print("The quotient of ",a/b)
+print("The quotient of ",a//b)
+print("The remainder of ",a%b)
+
